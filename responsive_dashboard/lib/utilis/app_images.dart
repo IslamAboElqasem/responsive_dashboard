d58a3eb1b9ccc6bf7e52d9a844d2a58a4b08de49
Islam Hesham <heshamislam556@gmail.com>
2024-07-09 10:39:28 -0700

    Add app images and shared preferences plugin registration
    Added app images to the project's assets and generated a Dart file to access them. Also registered the shared preferences plugin in the macOS GeneratedPluginRegistrant.swift file.

---
+++ responsive_dashboard/lib/utilis/app_images.dart
@@ -0,0 +1,64 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.png
+  static const String imagesAvatar1 = "assets/images/avatar_1.png";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar_2.png
+  static const String imagesAvatar2 = "assets/images/avatar_2.png";
+
+  /// Assets for imagesAvatar3
+  /// assets/images/avatar_3.png
+  static const String imagesAvatar3 = "assets/images/avatar_3.png";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.png
+  static const String imagesBalance = "assets/images/balance.png";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.png
+  static const String imagesCardBackground = "assets/images/card_background.png";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.png
+  static const String imagesDashboard = "assets/images/dashboard.png";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.png
+  static const String imagesExpenses = "assets/images/expenses.png";
+
+  /// Assets for imagesGallery
+  /// assets/images/gallery.png
+  static const String imagesGallery = "assets/images/gallery.png";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.png
+  static const String imagesIncome = "assets/images/income.png";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.png
+  static const String imagesLogout = "assets/images/logout.png";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/my_investments.png
+  static const String imagesMyInvestments = "assets/images/my_investments.png";
+
+  /// Assets for imagesMyTransctions
+  /// assets/images/my_transctions.png
+  static const String imagesMyTransctions = "assets/images/my_transctions.png";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.png
+  static const String imagesSettings = "assets/images/settings.png";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.png
+  static const String imagesStatistics = "assets/images/statistics.png";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet_account.png
+  static const String imagesWalletAccount = "assets/images/wallet_account.png";
+}
+

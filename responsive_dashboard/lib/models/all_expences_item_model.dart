2ac4ab8f13dab3ea503165df6476ab19bcbb7aea
Islam Hesham <heshamislam556@gmail.com>
2024-07-16 13:39:58 -0700

    Add AllExpencesItemModel class to models directory

---
+++ responsive_dashboard/lib/models/all_expences_item_model.dart
@@ -0,0 +1,11 @@
+class AllExpencesItemModel {
+  final String title;
+  final String image;
+  final String date;
+  final String price;
+  const AllExpencesItemModel(
+      {required this.title,
+      required this.image,
+      required this.date,
+      required this.price});
+}

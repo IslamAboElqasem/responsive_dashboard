1e953da32488a2b9a090fb79f4e04bb108597bc4
Islam Hesham <heshamislam556@gmail.com>
2024-07-26 01:36:17 -0700

    Refactor MyCardPageView and add MyCardSection widget
    This commit refactors MyCardPageView to accept a PageController as a required parameter and renames it to MyCardSection. It also adds a new MyCardPageView widget that uses the PageController to enable swiping between cards. Additionally, a DotsIndicator widget is added to show the current page index.

---
+++ responsive_dashboard/lib/views/widgets/my_card_section.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:responsive_dashboard/utilis/app_styles.dart';
+import 'package:responsive_dashboard/views/widgets/dots_indicator.dart';
+import 'package:responsive_dashboard/views/widgets/my_card_page_view.dart';
+
+class MyCardSection extends StatefulWidget {
+  const MyCardSection({super.key});
+
+  @override
+  State<MyCardSection> createState() => _MyCardSectionState();
+}
+
+class _MyCardSectionState extends State<MyCardSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(
+      () {
+        currentPageIndex = pageController.page!.round();
+        setState(() {});
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 420,
+          child: Text('My card', style: AppStyles.styleSemiBold20(context)),
+        ),
+        const SizedBox(height: 20),
+        MyCardPageView(
+          pageController: pageController,
+        ),
+        const SizedBox(height: 19),
+        DotsIndicator(
+          currentPageIndex: currentPageIndex,
+        )
+      ],
+    );
+  }
+}

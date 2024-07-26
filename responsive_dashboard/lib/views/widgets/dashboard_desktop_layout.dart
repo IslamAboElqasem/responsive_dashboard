6920e190c4b8c6d5478c0297e97add6c50293ab1
Islam Hesham <heshamislam556@gmail.com>
2024-07-26 00:07:10 -0700

    add import

--- responsive_dashboard/lib/views/widgets/dashboard_desktop_layout.dart
+++ responsive_dashboard/lib/views/widgets/dashboard_desktop_layout.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dashboard/views/widgets/all_expences_and_quick_invoice.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
-import 'package:responsive_dashboard/views/widgets/my_card.dart';
 import 'package:responsive_dashboard/views/widgets/my_card_page_view.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {

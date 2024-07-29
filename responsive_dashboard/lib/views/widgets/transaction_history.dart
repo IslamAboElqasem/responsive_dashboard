952f07eda5213d740f83bcd6bae2e6c207ae0bd6
Islam Hesham <heshamislam556@gmail.com>
2024-07-28 20:15:42 -0700

    Refactor transaction history UI and add transaction list view to transaction history

--- responsive_dashboard/lib/views/widgets/transaction_history.dart
+++ responsive_dashboard/lib/views/widgets/transaction_history.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:responsive_dashboard/utilis/app_styles.dart';
 import 'package:responsive_dashboard/views/widgets/Transaction_history_header.dart';
+import 'package:responsive_dashboard/views/widgets/transaction_history_list_view.dart';
 
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        TransactionHistoryHeader(),
+        const TransactionHistoryHeader(),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          '13 April 2022',
+          style: AppStyles.styleMedium16(context),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const TransactionHistoryListView()
       ],
     );
   }
